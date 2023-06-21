de0a7d7f600f7161646a955daf77518ff6e5fe49
DESKTOP-OHM2OB7\Alphadeveloper <alphadeveloper622@gmail.com>
2023-06-21 15:46:09 +0900

    init lambda

---
+++ src/check-user-exists/index.ts
@@ -0,0 +1,62 @@
+import * as AWS from 'aws-sdk';
+import { CONSTANTS } from '../_helpers';
+
+const cognito = new AWS.CognitoIdentityServiceProvider({ apiVersion: '2016-04-18' })
+
+const UserPoolId = CONSTANTS.USER_POOL_ID;
+
+export const handler = async (event: any = {}): Promise<any> => {
+
+
+    const usernameExist = await checkIfUsernameExist(event);
+    if (usernameExist) {
+        return prepareResponse('preferred_username')
+    }
+
+    const emailExist = await checkIfEmailExist(event);
+    if (emailExist) {
+        return prepareResponse('email')
+    }
+
+    return prepareResponse('')
+}
+
+
+const checkIfUsernameExist = async (event: any) => {
+    var params = {
+        UserPoolId,
+        Filter: 'preferred_username = "' + event.preferred_username + '"',
+        Limit: 1
+    };
+
+    const response = await cognito.listUsers(params).promise();
+
+    if (response.Users.length) {
+        return true;
+    }
+
+    return false;
+}
+
+
+const checkIfEmailExist = async (event: any) => {
+    var params = {
+        UserPoolId,
+        Filter: 'email = "' + event.email + '"',
+        Limit: 1
+    };
+
+    const response = await cognito.listUsers(params).promise();
+
+    if (response.Users.length) {
+        return true;
+    }
+
+    return false;
+}
+
+const prepareResponse = (attribute: any) => {
+    return {
+        attribute
+    }
+}
